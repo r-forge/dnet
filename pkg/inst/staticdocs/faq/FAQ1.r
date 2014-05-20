8b20fc6f652f3d7ea68bec7cd41aec7a85bf2fd9
hfang (Dr ) <hfang.shanghai@gmail.com>
2014-05-20 19:48:42 +0000

    Update to the version 1.0.3
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dnet@142 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 211add32d808dc0f25b09d6538f06334e56e0b22

--- pkg/inst/staticdocs/faq/FAQ1.r
+++ pkg/inst/staticdocs/faq/FAQ1.r
@@ -1,48 +1,5 @@
-# <span style="font-weight:bold; color:#F87217; text-decoration:underline">These built-in data are the backend of various analytical utilities supported in the dnet package, spanning a wide range of the known gene-centric knowledge across well-studied organisms.</span> They are provided as RData-formatted files which are regularly updated. Also, we will populate them by adding new knowledge, for example, upon request by users. The built-in RData are summarised in brief and available in the <a href="http://dnet.r-forge.r-project.org/rdata.html">RData</a> page.
+# <span style="font-weight:bold; color:#F87217; text-decoration:underline">The dnet package is distributed as a CRAN package, freely available both for academic and non-academic users.</span> For it to be working, the latest R version should be first installed, along with several reguired/suggested packages. The dnet package, like other R packages, can run on Windows, Mac and Linux, but some functionalities (such as parallel computing) are only supported in Linux and Mac. Details on how to install are described in the <a href="http://dnet.r-forge.r-project.org/rdata.html">Installation</a> page. Since the package is under active development, it is advisable to upgrade to the latest stable version by:
 
-# Usually, the users do not need to download them by self for use. Instead, the users are encouraged to understand what they want to use by simply looking up the keywords in the <a href="http://dnet.r-forge.r-project.org/docs.html">Documentations</a> page. The package has functions to import them or deal with them directly.
+install.packages("dnet",repos="http://cran.r-project.org",type="source")
 
-# <span style="font-weight:bold; color:#F87217; text-decoration:underline">The function dRDataLoader allows the users to import what they want to use.</span>
-
-# <span style="font-weight:bold; color:#F87217; text-decoration:underline">For the ease to use, organism-specific data start with 'org', followed by the specific organim ('Hs' for human), and the data content: only 'eg' means information about Entrez Genes, and further appendix (for example, 'GOBP') means information about their annotations by Gene Ontology Biological Process (GOBP).</span>
-
-## load human Entrez Genes (EG), and list the first 3 genes
-org.Hs.eg <- dRDataLoader(RData='org.Hs.eg')
-org.Hs.eg$gene_info[1:3,]
-## load annotations of human Entrez Genes by Gene Ontology Biological Process (GOBP), inspect the content and list the first 3 terms
-org.Hs.egGOBP <- dRDataLoader(RData='org.Hs.egGOBP')
-names(org.Hs.egGOBP)
-org.Hs.egGOBP$set_info[1:3,]
-## load annotations of human Entrez Genes by Disease Ontology (DO), inspect the content and list the first 5 terms
-org.Hs.egDO <- dRDataLoader(RData='org.Hs.egDO')
-org.Hs.egDO$set_info[1:3,]
-## load phylostratific age (PS) information of human Entrez Genes, inspect the content and list all our ancestors
-org.Hs.egPS <- dRDataLoader(RData='org.Hs.egPS')
-org.Hs.egPS$set_info
-## load domain superfamily (SF) information of human Entrez Genes, inspect the content and list the first 3 superfamilies
-org.Hs.egSF <- dRDataLoader(RData='org.Hs.egSF')
-org.Hs.egSF$set_info[1:3,]
-## load KEGG pathways for human Entrez Genes, inspect the content and list the first 3 pathways
-org.Hs.egMsigdbC2KEGG <- dRDataLoader(RData='org.Hs.egMsigdbC2KEGG')
-org.Hs.egMsigdbC2KEGG$set_info[1:3,]
-## load the network for human Entrez Genes as an 'igraph' object
-org.Hs.string <- dRDataLoader(RData='org.Hs.string')
-org.Hs.string
-## This network is extracted from the STRING database (version 9.1). Only those associations with medium confidence (score>=400) are retained. And the users can restrict to those edges with high confidence (score>=700, for example)
-network <- igraph::subgraph.edges(org.Hs.string, eids=E(org.Hs.string)[combined_score>=700])
-network
-
-# <span style="font-weight:bold; color:#F87217; text-decoration:underline">In addition to data import, the package has also functions (see below) to deal with them directly. In these functions, the users only need to specify which genome/organism and which ontology to use.</span>
-
-# Here, we use human TCGA mutation dataset as an example
-data(TCGA_mutations)
-symbols <- as.character(fData(TCGA_mutations)$Symbol)
-
-## Enrichment analysis using Disease Ontology (DO)
-data <- symbols[1:50] # select the first 50 human genes
-eTerm <- dEnricher(data, identity="symbol", genome="Hs", ontology="DO")
-
-## gene set enrichment analysis (GSEA) using KEGG pathways
-tol <- apply(exprs(TCGA_mutations), 1, sum) # calculate the total mutations for each gene
-data <- data.frame(tol=tol)
-eTerm <- dGSEA(data, identity="symbol", genome="Hs", ontology="MsigdbC2KEGG")
+# <span style="font-weight:bold; color:#F87217; text-decoration:underline">The dnet package is specifically designed to analyse high-throughput omics data in an integrative manner.</span> It has the focus on making sense of these digitised data from different angles including: integration with molecular networks, enrichments using ontologies, and relevance to gene evolutionary ages. New functionalities will be noticed under the section 'Features' in the <a href="http://dnet.r-forge.r-project.org">home</a> package. The workflow of the package can also be found in the <a href="http://dnet.r-forge.r-project.org/rdata.html">Installation</a> page.
