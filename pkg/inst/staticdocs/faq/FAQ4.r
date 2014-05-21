6760cd49e229843be6ea01ad46394febb3e18d08
hfang (Dr ) <hfang.shanghai@gmail.com>
2014-05-21 13:39:43 +0000

    Update to the version 1.0.3
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dnet@144 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: bfa720bd911dd314b19a2fb8fe4cff66c773a05d

--- pkg/inst/staticdocs/faq/FAQ4.r
+++ pkg/inst/staticdocs/faq/FAQ4.r
@@ -1,15 +1,11 @@
-# <span style="font-weight:bold; color:#F87217; text-decoration:underline">The dnet package supports two ways to visualise the identified networks/graphs: 1) the network itself as a single display, 2) the same network but with the multiple colorings/displays according to multiple samples.</span>
+# <span style="font-weight:bold; color:#F87217; text-decoration:underline">The dnet package supports two ways to visualise the identified networks/graphs: 1) the network itself as a single display, 2) the same network but with multiple colorings/displays according to samples.</span>
 
-# To demonstrate the visuals supported, we use a random graph according to the ER model, and only keep the largest component
+# To demonstrate the visuals supported, we use a random graph generated according to the ER model, and only keep the largest component.
 g <- erdos.renyi.game(100, 1/100)
 g <- dNetInduce(g, nodes_query=V(g))
 
-# <span style="font-weight:bold; color:#F87217; text-decoration:underline">To display the network itself, the key setting is the layout.</span> Since the network is represented as an object of class 'igraph', the layouts supported in the 'igraph' package can be found in the <a href="http://igraph.org/r/doc/layout.html">layout</a> page. 
-## using a force-based algorithm proposed by Fruchterman and Reingold
+# <span style="font-weight:bold; color:#F87217; text-decoration:underline">To display the network itself, the key setting is the layout.</span> Since the network is represented as an object of class 'igraph', the layouts supported in the 'igraph' package can be found in the <a href="http://igraph.org/r/doc/layout.html">layout</a> page. Below is an example using a force-based algorithm proposed by Fruchterman and Reingold:
 visNet(g, layout=layout.fruchterman.reingold)
-## using a spring embedder algorithm
-visNet(g, layout=layout.spring)
-
 # Additionally, the dnet package also uses two other layouts/diagrams: 1) arc diagram in one-dimensional layout, and 2) circle diagram. 
 
 # <span style="font-weight:bold; color:#F87217; text-decoration:underline">To better display the network, it is advisable to incorporate community information.</span> In doing so, we first identify communities based on a spin-glass model and simulated annealing.
@@ -39,3 +35,24 @@ mark.col <- visColoralpha(mcolors, alpha=0.2)
 mark.border <- visColoralpha(mcolors, alpha=0.2)
 edge.color <- c("grey", "black")[igraph::crossing(com,g)+1]
 visNet(g, colormap="rainbow", glayout=layout.fruchterman.reingold, vertex.color=vcolors, vertex.frame.color=vcolors, vertex.shape="sphere", mark.groups=mark.groups, mark.col=mark.col, mark.border=mark.border, mark.shape=1, mark.expand=10, edge.color=edge.color)
+
+# Now, let us look at <span style="font-weight:bold; color:#F87217; text-decoration:underline"> visualising the same network but with the multiple colorings/displays according to samples.</span>
+
+# Assume we have 10 samples, each containing numeric information about nodes in the graph:
+nnodes <- vcount(g)
+nsamples <- 10
+data <- matrix(runif(nnodes*nsamples), nrow=nnodes, ncol=nsamples)
+rownames(data) <- V(g)$name
+
+# <span style="font-weight:bold; color:#F87217; text-decoration:underline">In the dnet package, these sample-specific network visuals can be simply as they are provided, or samples being self-organised onto 2D landscape.</span>
+
+## simply as they are provided
+visNetMul(g, colormap="rainbow", data=data, glayout=layout.fruchterman.reingold)
+
+## being self-organised onto 2D sample landscape (ie a sheet-shape rectangle grid)
+sReorder <- dNetReorder(g, data, feature="node", node.normalise="none")
+visNetReorder(g, colormap="rainbow", data=data, sReorder)
+
+## By default, 2D sample landscape is built based on node features without considering node degrees. To take into account the connectivity in the network, the information used can be on edges which are transformed from information on nodes: input data and degree. In doing so, <span style="font-weight:bold; color:#F87217; text-decoration:underline">the transformed matrix of network edges × samples are used for self-organising samples onto 2D landscape (implemented in the 'supraHex' package).</span>
+sReorder <- dNetReorder(g, data, feature="edge", node.normalise="degree")
+visNetReorder(g, colormap="rainbow", data=data, sReorder)
